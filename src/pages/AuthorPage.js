c1a2f145a92c52287f83a2efd8aa05d8cf75e0ba
Coding4Toon <coding4toon@gmail.com>
2023-06-10 02:51:07 -0500

    Correct title

--- src/pages/AuthorPage.js
+++ src/pages/AuthorPage.js
@@ -6,7 +6,7 @@ const AuthorPage = () => {
             <Header />
             <div className="container">
                 <div className="inner-container">
-                    <h1 className="page-title">About this author : Toon</h1>
+                    <h1 className="page-title">About the author : Toon</h1>
                     <img src="./img/author.jpg" className="hero-image" alt="" />
                     <div className="blog-content">
                         <p className="blog-text">Meet Toon, an avid coder and knowledge sharer. With a passion for all things coding, Toon's blog posts are a treasure trove of insights and experiences. From tackling complex algorithms to exploring new technologies, Toon's articles offer valuable guidance and inspiration to fellow coding enthusiasts. Join Toon's journey and expand your coding horizons!</p>
